2b8f17f886a6254db57a4baf8c02720d11340561
kourosh sarabadani <kouroshtkk@gmail.com>
2026-06-07 13:59:05 +0200

    added square record

--- CircleRec.java
+++ CircleRec.java
@@ -1,4 +1,19 @@
 package shapes;
 
-public class CircleRec {
+public record CircleRec(int radius) implements Shape {
+    public CircleRec{
+        Shape.checkIsPositive(radius);
+    }
+    @Override
+    public double perimeter(){
+        return 2 * Math.PI * this.radius;
+    }
+    @Override
+    public double area() {
+        return Math.PI * this.radius * this.radius;
+    }
+    @Override
+    public String toString(){
+        return String.format("a circle of radius %d",radius);
+    }
 }
